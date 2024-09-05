3671ddefcdfdeb1ff0eacdd36af47776219d74cf
suryathota <surya.august5@gmail.com>
2024-09-05 11:03:28 +0000

    2nd commit of calculator.sh with substraction function

--- calculator.sh
+++ calculator.sh
@@ -2,10 +2,17 @@
 #author:surya-thota
 #date:05-09-2024
 #!/bin/bash
+
+#addittion function 
+
 read -p "enter A value " A
 read -p "enter B value " B
 add=$((A + B))
 echo  " addittion of A and B values are: $add "
 
+#substraction function
+
+sub=$(($A - $B))
+echo " substraction of A and B values are: $sub "
 
 
