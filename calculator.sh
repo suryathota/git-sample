2650e1d9a376550a0057802a199ea83a189a9bd7
suryathota <surya.august5@gmail.com>
2024-09-05 10:59:43 +0000

    calculator.sh with addittion function

---
+++ calculator.sh
@@ -0,0 +1,11 @@
+#description:script for calculator
+#author:surya-thota
+#date:05-09-2024
+#!/bin/bash
+read -p "enter A value " A
+read -p "enter B value " B
+add=$((A + B))
+echo  " addittion of A and B values are: $add "
+
+
+
